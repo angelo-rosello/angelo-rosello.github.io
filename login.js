a5f9a5349209de41374494b9e2b8702db8439d64
angelo-rosello <ang.rosello@gmail.com>
2024-08-29 17:23:48 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
     const hash = CryptoJS.SHA256(password).toString();
-    const correctHash = '1e9cc37678c0112d7a394909256c2a5998a6e57e2b0d82209900e1456e51b7eb'; // Mdp hashé
+    const correctHash = '804009875b8f147428508e89ac1fbcda6d9be01b56a5818e31b288b77706e61a'; // Mdp hashÃ©
     const guardImage = document.querySelector('.guard-image'); 
 
     if (hash === correctHash) {
@@ -10,12 +10,12 @@ function checkPassword() {
     } else {
         // Ajout de l'animation de tremblement
         guardImage.classList.remove('animate-zoomIn'); // Suppression de la classe animate-zoomIn
-        guardImage.classList.remove('shake'); // Assurez-vous que la classe est supprimée d'abord
-        void guardImage.offsetWidth; // Forcer le reflow en lisant une propriété
+        guardImage.classList.remove('shake'); // Assurez-vous que la classe est supprimÃ©e d'abord
+        void guardImage.offsetWidth; // Forcer le reflow en lisant une propriÃ©tÃ©
 
         guardImage.classList.add('shake');
 
-        // Enlever la classe shake après l'animation pour pouvoir la réutiliser
+        // Enlever la classe shake aprÃ¨s l'animation pour pouvoir la rÃ©utiliser
         setTimeout(() => {
             guardImage.classList.remove('shake');
         }, 500);
