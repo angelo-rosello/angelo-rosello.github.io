d6d5a14f5d95053c13087fc9cb11af55efb570f9
angelo-rosello <ang.rosello@gmail.com>
2024-09-17 20:06:04 +0200

    Update genererChapitres2.js

--- genererChapitres2.js
+++ genererChapitres2.js
@@ -10,9 +10,6 @@ function checkFileExists(url, callback) {
 // Fonction pour générer les liens des chapitres
 function genererLiensChapitre(chapitreNumero) {
     const ul = document.querySelector('.chapitres ul');
-    const li = document.createElement('li');
-    li.textContent = `Chapitre ${chapitreNumero} : `;
-
     const basePath = `https://angelo-rosello.github.io/doc-eleves/chapitre${chapitreNumero}/`;
 
     // Objets pour les noms de fichiers et les noms des liens
@@ -22,10 +19,17 @@ function genererLiensChapitre(chapitreNumero) {
         { file: 'corrige.pdf', label: 'Corrigé' }
     ];
 
+    let atLeastOneFileExists = false;
+    const li = document.createElement('li');
+    li.textContent = `Chapitre ${chapitreNumero} : `;
+
     files.forEach(({ file, label }) => {
         const filePath = basePath + file;
         checkFileExists(filePath, (exists) => {
             if (exists) {
+                if (!atLeastOneFileExists) {
+                    atLeastOneFileExists = true;
+                }
                 const a = document.createElement('a');
                 a.href = filePath;  // Lien vers le fichier
                 a.textContent = label;  // Nom du lien (Cours, Exercices, Corrigé)
@@ -33,10 +37,13 @@ function genererLiensChapitre(chapitreNumero) {
                 li.appendChild(a);
                 li.appendChild(document.createTextNode(' | '));
             }
+            if (label === 'Corrigé') {  // Après la vérification du dernier fichier
+                if (atLeastOneFileExists) {
+                    ul.appendChild(li);  // Ajouter le <li> à la liste si au moins un fichier existe
+                }
+            }
         });
     });
-
-    ul.appendChild(li);
 }
 
 // Appeler la fonction pour chaque chapitre
