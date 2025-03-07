449b8696cda68b15f4626e0415825fee8b70ee61
angelo-rosello <ang.rosello@gmail.com>
2025-03-07 18:49:55 +0100

    Update genererTP.js

--- genererTP.js
+++ genererTP.js
@@ -20,7 +20,7 @@ const tps = [
     { numero: 16, nom : "Calcul appproché d'intégrales", visible: true },
     { numero: 17, nom : "Conjecture de Syracuse", visible: true },
     { numero: 18, nom : "Variables aléatoires - Partie I", visible: true},
-    { numero: 19, nom : "Illustrations sur les séries", visible: true}
+    { numero: 19, nom : "Illustrations sur les séries", visible: true} 
     
     
     // Ajouter d'autres chapitres ici...
