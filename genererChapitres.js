d17d0056170cd647926bb13a6284d4d32dc8fad1
angelo-rosello <ang.rosello@gmail.com>
2025-05-16 19:54:08 +0200

    Update genererChapitres.js

--- genererChapitres.js
+++ genererChapitres.js
@@ -26,7 +26,7 @@ const chapitres = [
     { numero: 24, nom: "Somme de SEV", visible: true, afficherComplet : true,  afficherCorrige: true },
     { numero: 25, nom: "Intégrales impropres", visible: true,  afficherComplet : true, afficherCorrige: true }, 
     { numero: 26, nom: "Applications linéaires en dimension finie", visible: true, afficherComplet : true,  afficherCorrige: true },
-    { numero: 27, nom: "Dérivées successives, formules de Taylor", visible: true, afficherComplet : false,  afficherCorrige: false },
+    { numero: 27, nom: "Dérivées successives, formules de Taylor", visible: true, afficherComplet : true,  afficherCorrige: false },
     { numero: 28, nom: "Couples de variables aléatoires", visible: false, afficherComplet : false,  afficherCorrige: false },
     { numero: 29, nom: "Extrema et convexité", visible: false,  afficherComplet : false, afficherCorrige: false },
     { numero: 30, nom: "Convergences en probabilités", visible: false,  afficherComplet : false, afficherCorrige: false } 
